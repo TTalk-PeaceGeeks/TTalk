6b44db9a1b841099be811526d4f10f922d16bee2
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-28 16:34:14 -0800

    added some sign in logic decided it was awful

--- app/views/signup_view.js
+++ app/views/signup_view.js
@@ -1,3 +1,22 @@
 /**
  * Created by jlian on 2015-11-28.
  */
+var signUpView = Backbone.View.extend({
+
+    el: $("#sign_up"),
+
+    initialize: function(options)
+    {
+
+        // initialize position
+        this.$el.css({display: 'none', right: '200px', top: '1200px'}, 2000);
+
+        this.render();
+    },
+
+    render: function()
+    {
+        return this;
+    }
+}
+
